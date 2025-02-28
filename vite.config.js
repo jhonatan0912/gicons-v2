57973a03a22544cd62e212621ef10819b4620b13
Jhonatan <jhoonatan_0912@hotmail.com>
2025-02-28 02:13:02 -0500

    Revert "Updates"
    This reverts commit 04bdea85e40729ded1cdca68081dbd83d25223e0.

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import { createVuePlugin } from "vite-plugin-vue2";
 
 export default defineConfig({
-  base: "./",
   plugins: [createVuePlugin()],
   build: {
     lib: {
