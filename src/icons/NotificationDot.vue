340814f33cbed373df77210e88deb807d1df4cf6
Jhonatan <jhoonatan_0912@hotmail.com>
2025-03-01 11:05:48 -0500

    Add New Icons

---
+++ src/icons/NotificationDot.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="g-icon">
+    <svg viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M8.99717 1.88562C5.67145 1.88562 4.22645 4.89705 4.22645 6.88776C4.22645 8.37562 4.44217 7.93776 3.61859 9.75276C2.61288 12.3392 6.65717 13.3963 8.99717 13.3963C11.3365 13.3963 15.3807 12.3392 14.3757 9.75276C13.5522 7.93776 13.7679 8.37562 13.7679 6.88776C13.7679 4.89705 12.3222 1.88562 8.99717 1.88562Z"
+        stroke="currentColor"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M10.7296 15.3842C9.75893 16.4684 8.24465 16.4813 7.26465 15.3842"
+        stroke="currentColor"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  </div>
+</template>
