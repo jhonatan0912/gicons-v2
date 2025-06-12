4f718772607ad4e9b61857a97e59659122e14490
Jhonatan <jhoonatan_0912@hotmail.com>
2025-06-12 14:07:51 -0500

    Add New Icons

---
+++ src/icons/Voice3.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="g-icon">
+    <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M11.9998 14.8481V14.8481C9.75662 14.8481 7.93774 13.0218 7.93774 10.7682V6.08095C7.93774 3.82732 9.75662 2 11.9998 2C14.2441 2 16.0619 3.82732 16.0619 6.08095V10.7682C16.0619 13.0218 14.2441 14.8481 11.9998 14.8481Z"
+        stroke="currentColor"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M11.9998 22V18.8391M11.9998 18.8391C16.4191 18.8391 20 15.2395 20 10.8007M11.9998 18.8391C7.58148 18.8391 4 15.2395 4 10.8007"
+        stroke="currentColor"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  </div>
+</template>
